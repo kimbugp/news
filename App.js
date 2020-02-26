28a905d491494b69de503371e9ef7a0d9fc499ff
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-26 11:36:59 +0300

    add headlines news feed

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import { Provider } from "react-redux";
+
+import Router from "./app/index";
+import store from "./app/redux/store";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,3 @@
+import Router from "./config/routes";
+
+export default Router;
